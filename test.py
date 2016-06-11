24d7af51a4f4bf9550ae65cbbeedb33344ffc940
Rahul Singh <Rahulsingh@Rahuls-MacBook-Air.local>
2016-06-11 20:36:09 +0530

    yay

--- test.py
+++ test.py
@@ -1,18 +1,1 @@
-import smtplib
-
-sender = 'sau.ag01@gmail.com'
-receivers = ['eb31rahul@gmail.com']
-
-message = """From: From Person <sau.ag01@gmail.com>
-To: To Person <eb31rahul@gmail.com>
-Subject: SMTP e-mail test
-
-This is a test e-mail message.
-"""
-
-try:
-   smtpObj = smtplib.SMTP('localhost')
-   smtpObj.sendmail(sender, receivers, message)         
-   print "Successfully sent email"
-except SMTPException:
-   print "Error: unable to send email"
+print 'hello World'
